99b46d434a9bc0fb2d5f08d9357a4542f5f0f174
s-arslaan <shaikharslaan1234@gmail.com>
2021-07-09 01:06:58 +0530

    expenses filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import { useState } from "react";
+import React from "react";
+
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // useState is required to reload this particular component when onClick occurs
-  const [title, setTitle] = useState(props.title);
-
-  // let title = props.title;
-
-  const clickHandler = () => {
-    setTitle("Updated Title");
-    // title = 'New Title';
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,24 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const saveYearHandler = (enteredYear) => {
+    // console.log(enteredYear);
+    setFilteredYear(enteredYear);
+    // props.onFilterYear(enteredYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter 
+        selected={filteredYear}
+        onSaveYear={saveYearHandler} 
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
